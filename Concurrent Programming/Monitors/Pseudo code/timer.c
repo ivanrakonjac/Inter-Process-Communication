3bab60869dfeddeafa84245028c376926cdb52c2
Ivan_Rakonjac <rakonjacivan98@gmail.com>
2020-04-03 23:01:53 +0200

    Monitors: izmenjen timer.c

--- Concurrent Programming/Monitors/Pseudo code/timer.c
+++ Concurrent Programming/Monitors/Pseudo code/timer.c
@@ -1,5 +1,8 @@
 //Disciplina: SW
 
+//Izmena: tick budi samo prvi proces iz time queuea, a onda taj probudjeni proverava ima li jos koga, a da ga treba probuditi
+//To se ponavlja dokle god ima procea koje treba buditi
+
 monitor Sleep{
 
 	int now = 0;
@@ -9,12 +12,13 @@ monitor Sleep{
 		if(n>0){
 			long wakeUp = now + n;
 			time.wait(wakeUp);
+			if(!time.empty() && time.minRank()<=now) time.singal();
 		}
 	}
 
 
 	void tick(){//tick poziva hardver
 		now++;
-		while(!time.empty() && time.minRank()<=now) time.singal();
+		if(!time.empty() && time.minRank()<=now) time.singal();
 	}
 }
