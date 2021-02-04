c968b37b43db9c36989da99dfb0dfd2d119fcfbf
Ivan_Rakonjac <rakonjacivan98@gmail.com>
2021-02-04 17:36:39 +0100

    Hadoop: male izmene na Filmovi1.java

--- Hadoop MapReduce/Max broj reditelja/mapReduce/Filmovi1.java
+++ Hadoop MapReduce/Max broj reditelja/mapReduce/Filmovi1.java
@@ -15,18 +15,29 @@ import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 
-public class Filmovi1 {
+/**
+ * 
+ * @author Ika
+ * 
+ * Trazimo film sa maksimalnim brojem reditelja, ikada
+ * 
+ * baza sa adrese: https://datasets.imdbws.com/
+ * 
+ * pa title.crew.tsv.gz
+ *
+ */
 
+public class Filmovi1 {
+	
 	//Mapper<KEYIN, VALUEIN, KEYOUT, VALUEOUT>
-	//KEYIN ako citamo iz textualnog fajla liniju po liniju == LongWritable
+	//KEYIN ako zelimo da citamo iz textualnog fajla indexirano liniju po liniju == LongWritable
 	//VALUEIN je Text, jer iz ulaznog fajla citamo liniju po liniju
 	//KEYOUT - sta je izlazni kljuc, ako ne znam sta je => Text
 	//VALUEOUT - izlazna vrednost, ako ne znam sta je => Text
 	public static class FilmMapper extends Mapper<LongWritable, Text, Text, Text>{
-
 		
 		int max = 0;
-		
+
 		@Override
 		protected void map(LongWritable key, Text value, Mapper<LongWritable, Text, Text, Text>.Context context)
 				throws IOException, InterruptedException {
@@ -36,57 +47,59 @@ public class Filmovi1 {
 			
 			String title = data[0];
 			String[] directors = data[1].split(",");
-			String[] writers = data[2].split(",");
+			String[] writers = data[1].split(",");
 			
 			int directorCnt = directors.length;
 			
-			if(directorCnt>max) {
+			if(directorCnt > max) {
 				max = directorCnt;
-				System.out.println("Novi max:" + max);
 			}
 			
 			//prosledjujem lokalne maximume
-			if(directorCnt == max) {
-				Text key1 = new Text("max"); //svi sa istim keyem zavrsavaju na istom racunaru
-				Text value1 = new Text(max + "\t" + title);
+			if(max==directorCnt) {
+				
+				Text key1 = new Text ("max");
+				Text value1 = new Text (max + "\t" + title);
 				context.write(key1, value1);
 				
 				System.out.println("Poslao sam: " + key1 + " " + value1);
+
 			}
+			
+			
+					
 		}
 		
 	}
 	
-	public static class FilmReducer extends Reducer<Text, Text, Text, Text>{
+	
+	public static class  FilmReducer extends Reducer<Text, Text, Text, Text>{
 
-		//od razlicitih lokalnih maksimuma koje su nasli razliciti racunari treba naci globalni maximum
 		@Override
 		protected void reduce(Text key, Iterable<Text> values, Reducer<Text, Text, Text, Text>.Context context)
 				throws IOException, InterruptedException {
 			
-			System.out.println("Usao sam u Reducer!");
-			
 			int max = 0;
 			
-			List<String> maxDirectorsFilms = new LinkedList<>(); 
+			List<String> maxDirectorsFilms = new LinkedList<>();
 			
-			for(Text value: values) {
+			for (Text value : values) {
+				
 				String line = value.toString();
 				String[] data = line.split("\t");
-				
 				int numDirectors = Integer.parseInt(data[0]);
 				String title = data[1];
 				
-				//ako je nadjeni numDirectors > trenutnog max => zameni i isprazni listu
-				if(numDirectors>max) {
+				// ako je nadjeni numDirectors > trenutnog max => zameni i isprazni listu
+				if(numDirectors > max) {
 					max = numDirectors;
 					maxDirectorsFilms.clear();
 				}
 				
-				//ako je numDirectors==max dodaj ime filma u listu
-				if(numDirectors==max) {
+				if(max==numDirectors) {
 					maxDirectorsFilms.add(title);
 				}
+						
 			}
 			
 			//ispisi listu u context
@@ -99,13 +112,15 @@ public class Filmovi1 {
 		}
 		
 	}
-	
+
 	public static void main(String[] args) throws IOException, ClassNotFoundException, InterruptedException {
+		
 		Job job = Job.getInstance();
-		//ime da bi mogli da pratimo posao u nekom sistemu
+		// ime ako zelimo da pratimo nas posao na nekom distribuiranom sistemu
 		job.setJobName("Filmovi sa max brojem reditelja");
-		//sve sto treba da se distribuira na druge racunare se nalazi u ovoj klasi
-		job.setJarByClass(Filmovi1.class);
+		
+		// sve sto treba da se dalje distribuira se nalazi unutar ovog 1 naseg fajla (klase)
+		job.setJarByClass(Filmovi1.class); 
 		
 		//ulaz u nas sistem je tekstualnog tipa, tj. kako nam cita objekte iz ulaznog toga => kao najobicniji text
 		job.setInputFormatClass(TextInputFormat.class);
@@ -127,9 +142,11 @@ public class Filmovi1 {
 		//gde na kraju upisujemo podatke
 		FileOutputFormat.setOutputPath(job, new Path(args[1]));
 		
+		//cekamo dok se posao ne zavrsi
 		job.waitForCompletion(true);
 		
 		System.out.println("Test " + args[0]);
+		
 	}
 
 }
